07899cba8ca5ec6ef99969cbc6b2c104d78c8fc0
ohkyungheon <kho3455@naver.com>
2021-05-30 20:38:06 +0900

    援ш?留??뚯뒪肄붾뱶 ?앹꽦

--- app/src/main/java/com/example/registerloginexample/Googlemap.java
+++ app/src/main/java/com/example/registerloginexample/Googlemap.java
@@ -1,14 +1,46 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class Googlemap extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Googlemap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_googlemap);
+
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.644211, 126.669103); // 장기역 위치
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("장기역");
+        markerOptions.snippet("지하철역");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "android add xml"
+git commit -m "구글맵 소스코드 생성"
 git push origin master
